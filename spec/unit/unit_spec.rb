879bd00a1553d340e749f2fdc0a3a1858fb11244
root <root@localhost>
2021-09-27 03:19:58 +0000

    author unit test

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,18 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: 'jk rowling')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end
