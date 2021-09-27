404c492bc8975e5010e9d278e07a3a0d753d5cf4
root <root@localhost>
2021-09-27 06:24:06 +0000

    working on testing

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe 'Creating a book', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'author', with: 'jk rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('jk rowling')
+  end
+
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'price', with: 5.99
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(5.99)
+  end
+
+  scenario 'valid publish date' do
+    visit new_book_path
+    fill_in 'publishedDate', with: '01-01-2021'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('01-01-2021')
+  end
+
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -14,9 +14,8 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
-end
 
-RSpec.describe Book, type: :model do
+  #
   subject do
     described_class.new(author: 'jk rowling')
   end
@@ -25,8 +24,37 @@ RSpec.describe Book, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without an author' do
     subject.author = nil
     expect(subject).not_to be_valid
   end
+
+  #
+  subject do
+    described_class.new(price: 5.99)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  #
+  subject do
+    described_class.new(publishedDate: '01-01-2021')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a publish date' do
+    subject.publishedDate = nil
+    expect(subject).not_to be_valid
+  end
+
 end
