17b77778db891a2417e0517a92523784bed00324
root <root@localhost>
2021-09-30 16:47:17 +0000

    actual working tests. final touch ups

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,39 +2,52 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'jk rowling'
     fill_in 'Price', with: 15.99
-    fill_in 'book[published_date]', with: '01-01-2021'
+    select '2020', :from => 'book_published_date_1i'
+    select 'January', :from => 'book_published_date_2i'
+    select '10', :from => 'book_published_date_3i'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
   end
 
-  # scenario 'valid author' do
-  #   visit new_book_path
-  #   fill_in 'author', with: 'jk rowling'
-  #   click_on 'Create Book'
-  #   visit books_path
-  #   expect(page).to have_content('jk rowling')
-  # end
-  #
-  # scenario 'valid price' do
-  #   visit new_book_path
-  #   fill_in 'price', with: 5.99
-  #   click_on 'Create Book'
-  #   visit books_path
-  #   expect(page).to have_content(5.99)
-  # end
-  #
-  # scenario 'valid publish date' do
-  #   visit new_book_path
-  #   fill_in 'published_date', with: '01-01-2021'
-  #   click_on 'Create Book'
-  #   visit books_path
-  #   expect(page).to have_content('01-01-2021')
-  # end
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Price', with: 15.99
+    select '2020', :from => 'book_published_date_1i'
+    select 'January', :from => 'book_published_date_2i'
+    select '10', :from => 'book_published_date_3i'
+    click_on 'Create Book'
+    expect(page).to have_content('jk rowling')
+  end
 
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Price', with: 15.99
+    select '2020', :from => 'book_published_date_1i'
+    select 'January', :from => 'book_published_date_2i'
+    select '10', :from => 'book_published_date_3i'
+    click_on 'Create Book'
+    expect(page).to have_content('15.99')
+  end
+
+  scenario 'valid date' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Price', with: 15.99
+    select '2020', :from => 'book_published_date_1i'
+    select 'January', :from => 'book_published_date_2i'
+    select '10', :from => 'book_published_date_3i'
+    click_on 'Create Book'
+    expect(page).to have_content('2020-01-10')
+  end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,16 +3,46 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter', author: 'jk rowling', price: 15.99, published_date: '01/01/2021')
+    described_class.new(title: 'harry potter', author: 'jk rowling', price: 15.99, published_date: '2021-04-01')
   end
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a title' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+  # test 2 do
+  #   described_class.new(title: 'The Giver', author: 'Lois Lowry', price: 20, published_date: '1993-05-10')
+  # end
+  #
+  # it 'is valid with valid attributes' do
+  #   expect(test2).to be_valid
+  # end
+  #
+  # it 'is not valid without all attributes' do
+  #   test2.title = nil
+  #   test2.author = nil
+  #   test2.price = nil
+  #   test2.published_date = nil
+  #   expect(test2).not_to be_valid
+  # end
 
 end
